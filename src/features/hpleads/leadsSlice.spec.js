36ba8f52f0f352f517ac367884b4ee4910366c3a
Michael Trenggono <michael.trenggono@ibuildnew.com.au>
2021-06-22 13:38:37 +1000

    Fix Failing test

--- src/features/hpleads/leadsSlice.spec.js
+++ src/features/hpleads/leadsSlice.spec.js
@@ -4,12 +4,14 @@ import leadsReducer, {
 
 describe('Should fetch leads asynchronously', function () {
     const initialState = {
+        initialLeads: [],
         leads: [],
         status: 'idle',
     };
 
     it('should handle initial state', () => {
         expect(leadsReducer(undefined, { type: 'unknown' })).toEqual({
+            initialLeads: [],
             leads: [],
             status: 'idle',
         });
@@ -24,6 +26,6 @@ describe('Should fetch leads asynchronously', function () {
     it('should have leads when finished', () => {
         const action = {type: fetchLeadsAsync.fulfilled.type, payload: [2, 3]};
         const state = leadsReducer(initialState, action);
-        expect(state).toEqual({leads: [2, 3], status: 'idle' });
+        expect(state).toEqual({initialLeads: [2, 3], leads: [2, 3], status: 'idle' });
     });
 });
